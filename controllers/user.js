f36e019f670b5271d6fa282c0a00ea96797443f2
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-10-20 12:59:59 +0200

    Updated the signup user controller

---
+++ controllers/user.js
@@ -0,0 +1,23 @@
+const User = require("../schemas/User");
+const jwt = require("jsonwebtoken");
+
+// CreateToken
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
+
+// Signup user
+const userSignUp = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    //create token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { userSignUp };
