940aa39616d3232bdfeb85079d836163e6a9ad05
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-10-26 14:54:54 +0200

    Fixed the token image of user

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,10 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const { userLocation } = require("../utils/location");
 
-const createToken = (_id, name) => {
-  return jwt.sign({ _id, name }, process.env.SECRET, { expiresIn: "1d" });
+const createToken = (_id, name, image) => {
+  return jwt.sign({ _id, name, image }, process.env.SECRET, {
+    expiresIn: "1d",
+  });
 };
 //login user
 const loginUser = async (req, res) => {
@@ -22,8 +24,6 @@ const loginUser = async (req, res) => {
     } else {
       token = createToken(user._id, user.username);
     }
-
-    // const token = createToken(user._id);
 
     res.status(200).json({ email, token });
     console.log("success with login");
@@ -82,8 +82,15 @@ const userSignUp = async (req, res) => {
   const userInfo = req.body;
   try {
     const user = await User.signup(userInfo);
-    //create token
-    const token = createToken(user._id, userInfo.username);
+    //creating token
+    let token;
+    if (user.userInfo.userImage) {
+      console.log(user.userInfo.userImage);
+      token = createToken(user._id, user.username, user.userInfo.userImage);
+    } else {
+      token = createToken(user._id, user.username);
+    }
+    console.log(token);
     res.status(200).json({ email: userInfo.email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
