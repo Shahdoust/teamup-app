41bc45d161809207561ae8de580b5931e1f95358
KateOblomova <kate.oblom@gmail.com>
2023-11-01 14:09:43 +0100

    edit picture on path user/upload/:id

--- routes/user.js
+++ routes/user.js
@@ -17,14 +17,14 @@ const upload = require("../service/upload");
 const app = express.Router();
 app.post("/signup", userSignUp);
 // app.post("/signup", upload.single("picture"), uploadImage);
-// app.post("/upload/:id", upload.single("picture"), uploadImage);
-
+// console.log(upload.single());
 app.post("/login", loginUser);
 app.post("/login/:id/reset-password", resetPasswordUser);
 app.get("/users", viewAllUserProfile);
 app.get("/users/:id", viewOneUserProfile);
 app.put("/edit/:id", editUserInfo);
-app.put("/edit/:id", upload.single("picture"), uploadImage); //upload image and update user
+app.post("/upload/:id", upload.single("picture"), uploadImage);
+//app.put("/edit/:id", upload.single("picture"), uploadImage); //upload image and update user
 app.post("/edit/:id", createUserCoordinates);
 const checkAuth = require("../middlewares/checkAuth");
 app.post("/users/:userId/rater-user", checkAuth, submitUserRating);
