e6d9e5f79bf7608753eb6e19e157831b28e2dfd7
KateOblomova <kate.oblom@gmail.com>
2023-11-06 14:29:21 +0100

    new endpoint to edit sports

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,7 @@ const {
   submitUserRating,
   uploadImage,
   deleteUser,
+  editSportsFollowed,
 } = require("../controllers/user");
 const checkToken = require("../middlewares/checkAuth");
 const upload = require("../service/upload");
@@ -22,6 +23,7 @@ app.post("/login", loginUser);
 app.post("/login/:id/reset-password", resetPasswordUser);
 app.get("/users", viewAllUserProfile);
 app.get("/users/:id", viewOneUserProfile);
+app.put("/edit/sports/:id", editSportsFollowed); //try to delete sports
 app.put("/edit/:id", editUserInfo);
 app.post("/upload/:id", upload.single("picture"), uploadImage);
 //app.put("/edit/:id", upload.single("picture"), uploadImage); //upload image and update user
