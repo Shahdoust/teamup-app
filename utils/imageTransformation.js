82d0a840f03950000c6b0be6c5e3555c851eced9
KateOblomova <kate.oblom@gmail.com>
2023-11-07 17:32:23 +0100

    passes pattern only once

--- utils/imageTransformation.js
+++ utils/imageTransformation.js
@@ -1,35 +1,64 @@
 const User = require("../schemas/User");
 
-const changeImageLink = async (req, res) => {
+// const changeImageLink = async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     const pattern = /upload/;
+//     function insert(str, index, value) {
+//       return str.substr(0, index) + value + str.substr(index);
+//     }
+//     const usersWithImage = users.map(async (user) => {
+//       if (
+//         user.userInfo.userImage &&
+//         user.userInfo.userImage?.includes("upload/")
+//       ) {
+//         const index = user.userInfo.userImage?.indexOf("upload/");
+
+//         const link = insert(
+//           user.userInfo.userImage,
+//           index + 7,
+//           "c_fill,g_face,h_400,w_400/f_png/r_max/"
+//         );
+//         const updatedUser = await User.findOneAndUpdate(
+//           { "userInfo.userImage": user.userInfo.userImage },
+//           {
+//             $set: { "userInfo.userImage": link },
+//           },
+//           { new: true }
+//         );
+//         return updatedUser;
+//       }
+//     });
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+const changeImageLink = async (user) => {
   try {
-    const users = await User.find();
-    const pattern = /upload/;
     function insert(str, index, value) {
       return str.substr(0, index) + value + str.substr(index);
     }
-    const usersWithImage = users.map(async (user) => {
-      if (
-        user.userInfo.userImage &&
-        user.userInfo.userImage?.includes("upload/")
-      ) {
-        const index = user.userInfo.userImage?.indexOf("upload/");
+    if (
+      user.userInfo.userImage &&
+      user.userInfo.userImage?.includes("upload/")
+    ) {
+      const index = user.userInfo.userImage?.indexOf("upload/");
 
-        const link = insert(
-          user.userInfo.userImage,
-          index + 7,
-          "c_fill,g_face,h_400,w_400/f_png/r_max/"
-        );
-        console.log("link after", link);
-        const updatedUser = await User.findOneAndUpdate(
-          { "userInfo.userImage": user.userInfo.userImage },
-          {
-            $set: { "userInfo.userImage": link },
-          },
-          { new: true }
-        );
-        return updatedUser;
-      }
-    });
+      const link = insert(
+        user.userInfo.userImage,
+        index + 7,
+        "c_fill,g_face,h_400,w_400/f_png/r_max/"
+      );
+      const updatedUser = await User.findOneAndUpdate(
+        { "userInfo.userImage": user.userInfo.userImage },
+        {
+          $set: { "userInfo.userImage": link },
+        },
+        { new: true }
+      );
+      return updatedUser;
+    }
   } catch (err) {
     console.log(err.message);
   }
